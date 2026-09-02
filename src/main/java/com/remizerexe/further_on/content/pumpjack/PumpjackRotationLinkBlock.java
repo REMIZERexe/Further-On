a425aca68bb7d6be597091cc105e952bcdf1e4bc
REMIZERexe <samira29082015@gmail.com>
2026-09-02 13:56:04 +0200

    Minor fix with the pumpjack model
    nothing really

--- src/main/java/com/remizerexe/further_on/content/pumpjack/PumpjackRotationLinkBlock.java
+++ src/main/java/com/remizerexe/further_on/content/pumpjack/PumpjackRotationLinkBlock.java
@@ -1,9 +1,28 @@
 package com.remizerexe.further_on.content.pumpjack;
 
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
 public class PumpjackRotationLinkBlock extends Block {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
     public PumpjackRotationLinkBlock(Properties properties) {
         super(properties);
+        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(FACING, context.getHorizontalDirection());
     }
 }
